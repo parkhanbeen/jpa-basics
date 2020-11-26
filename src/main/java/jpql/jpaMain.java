3587653587bf694979d656326eef209fd46be57e
박한빈 <phb0228@naver.com>
2020-11-27 00:35:36 +0900

    JPQL 타입 표현과 조건식 예제

--- src/main/java/jpql/jpaMain.java
+++ src/main/java/jpql/jpaMain.java
@@ -3,6 +3,7 @@ package jpql;
 import javax.persistence.*;
 import java.util.List;
 
+
 public class jpaMain {
 
     public static void main(String[] args) {
@@ -14,62 +15,68 @@ public class jpaMain {
         tx.begin();
 
         try{
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
-            for(int i =0; i<100; i++){
+            Member member = new Member();
+            member.setUsername("관리자");
+            member.setAge(20);
+            member.setTeam(team);
+            member.setType(MemberType.ADMIN);
 
-                Team team = new Team();
-                team.setName("team"+i);
-                em.persist(team);
-
-                Member member = new Member();
-                member.setUsername("member"+i);
-                member.setAge(i+1);
-                member.setTeam(team);
-
-                em.persist(member);
-            }
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            // 페이징 쿼리 // 각각 데이터 베이스의 방언에 맞춰 쿼리가 나간다.
-            List<Member> result = em.createQuery("select m from Member m inner join m.team t")
-                    .setFirstResult(1)
-                    .setMaxResults(10)
+            // JPQL 타입 표현
+            // ENUM 타입은 패키지명을 모두 포함한다.
+            String query = "select m.username, 'HELLO', true From Member m " +
+                    "where m.type = :userType";
+            List<Object[]> result = em.createQuery(query)
+                    .setParameter("userType",MemberType.ADMIN)
                     .getResultList();
 
-            System.out.println("result.size = " + result.size());
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
+            for (Object[] objects : result) {
+                System.out.println("objects[0] = " + objects[0]);
+                System.out.println("objects[0] = " + objects[1]);
+                System.out.println("objects[0] = " + objects[2]);
+
             }
 
-            // 조인
-            // 내부 조인
-            String innerQuery = "select m from Member m inner join m.team t";
-            List<Member> innerResult = em.createQuery(innerQuery,Member.class)
+            // 조건식 - case식
+            String caseQuery =
+                    "select " +
+                            "case when m.age <= 10 then '학생요금' " +
+                            "     when m.age >= 60 then '경로요금' " +
+                            "     else '일반요금'" +
+                            "end " +
+                            "from Member m";
+            List<String> resultList = em.createQuery(caseQuery,String.class)
                     .getResultList();
 
-            // 외부 조인
-            // outer 생략가능
-            String outerQuery = "select m from Member m left outer join m.team t";
-            List<Member> outerResult = em.createQuery(outerQuery,Member.class)
-                    .getResultList();
+            for (String s : resultList) {
+                System.out.println("s = " + s);
 
-            // 세타 조인
-            // cross join 으로 나옴
-            String query = "select m from Member m, Team t where m.username = t.name";
-            List<Member> joinResult = em.createQuery(query,Member.class)
-                    .getResultList();
+            }
 
-            System.out.println("joinResult.size() = " + joinResult.size());
-
-            // on절
-            String onQuery = "select m from Member m left join m.team t on t.name = 'team20'";
-            List<Member> onResult = em.createQuery(onQuery,Member.class)
+            // 모든 db에서 사용가능.
+            // COALESCE: 하나씩 조회해서 null이 아니면 반환
+            String coalQuery = "select coalesce(m.username, '이름 없는 회원') as username from Member m ";
+            List<String> coalList = em.createQuery(coalQuery,String.class)
                     .getResultList();
 
 
+            // NULLIF: 두 값이 같으면 null 반환, 다르면 첫번째 값 반환
+            String nullifQuery = "select nullif(m.username, '관리자') as username from Member m ";
+            List<String> nullifList = em.createQuery(nullifQuery,String.class)
+                    .getResultList();
 
+            for (String s : nullifList) {
+                System.out.println("s = " + s);
+
+            }
 
             tx.commit();
 
